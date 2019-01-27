f4398994e68a41048bbf758e8f20765aa3856581
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-27 21:51:10 +0900

    add root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'web#index'
   get 'web/index'
+  resources :zips
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

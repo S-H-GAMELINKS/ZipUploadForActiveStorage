419fc61f334227d60d92ede22dc2b23d980e297a
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-27 21:53:32 +0900

    remove some files

--- app/controllers/zips_controller.rb
+++ app/controllers/zips_controller.rb
@@ -1,6 +1,6 @@
 class ZipsController < ApplicationController  
     def create
-      @zip = Zip.create params.require(:zip).permit(:files)
+      @zip = Zip.create params.require(:zip).permit(:file)
       redirect_to root_path
     end
 end
--- app/models/zip.rb
+++ app/models/zip.rb
@@ -1,3 +1,3 @@
 class Zip < ApplicationRecord
-    has_many_attached :files
+    has_one_attached :file
 end

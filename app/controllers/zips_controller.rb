8edc7e11c2f5c375af02448913978160558c90b6
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-27 21:51:42 +0900

    uploads files

--- app/controllers/zips_controller.rb
+++ app/controllers/zips_controller.rb
@@ -1,6 +1,6 @@
 class ZipsController < ApplicationController  
     def create
-      @zip = Zip.create params.require(:zip).permit(:file)
+      @zip = Zip.create params.require(:zip).permit(:files)
       redirect_to root_path
     end
 end
--- app/models/zip.rb
+++ app/models/zip.rb
@@ -1,3 +1,3 @@
 class Zip < ApplicationRecord
-    has_one_attached :file
+    has_many_attached :files
 end
